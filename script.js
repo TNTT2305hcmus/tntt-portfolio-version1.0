1cdda0e50e53acdd33691ba198b8dcdbd5f1d5eb
TRI THANH <trannguyentrithanh38@gmail.com>
2025-10-03 17:57:18 +0700

    Catch event scroll when click navbar

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
